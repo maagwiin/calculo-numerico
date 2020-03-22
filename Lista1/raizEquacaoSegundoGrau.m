015f346be1793852e517c659aa42d74fe8fe551b
maagwiin <magnuwindell@gmail.com>
2020-03-21 21:25:04 -0300

    done

--- Lista1/raizEquacaoSegundoGrau.m
+++ Lista1/raizEquacaoSegundoGrau.m
@@ -6,7 +6,7 @@
 function [x1, x2] = raizEquacaoSegundoGrau(a, b, c)
     delta = b^2 - 4*a*c;
 
-    x1 = (-b + sqrt(delta))/2*a;
-    x2 = (-b - sqrt(delta))/2*a;
+    x1 = (-b + sqrt(delta))/(2*a);
+    x2 = (-b - sqrt(delta))/(2*a);
     return;
 end
